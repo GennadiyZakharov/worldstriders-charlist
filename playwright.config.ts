f58b4b8707a5609246f0014d33aaaf5caffdb7ce
Gennadii Zakharov <Gennadiy.Zakharov@gmail.com>
2026-03-20 09:03:31 +0000

    Fixed folder location for playwright screenshots and report.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
+  outputDir: "artifacts/test-results",
+  snapshotPathTemplate: "artifacts/{testFilePath}-snapshots/{arg}{-projectName}{-platform}{ext}",
   fullyParallel: true,
   timeout: 30_000,
   retries: process.env.CI ? 2 : 0,
-  reporter: [["list"], ["html", { open: "never" }]],
+  reporter: [["list"], ["html", { open: "never", outputFolder: "artifacts/playwright-report" }]],
   use: {
     baseURL: "http://127.0.0.1:4173",
     trace: "on-first-retry"
